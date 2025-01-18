17807a1e75e81eb18b7beed8934a946924c5be1b
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2025-01-18 13:45:45 +0100

    actual singleton now

--- Singleton Pattern/src/PRINTSPOOOLERLELERLELLERLER/Main.java
+++ Singleton Pattern/src/PRINTSPOOOLERLELERLELLERLER/Main.java
@@ -2,7 +2,7 @@ package PRINTSPOOOLERLELERLELLERLER;
 
 public class Main {
     public static void main(String[] args) {
-        SpooleOfThePrint p = new SpooleOfThePrint();
+        SpooleOfThePrint p = SpooleOfThePrint.getInstance();
         CanYouTellImLosingMyMind d1 = new CanYouTellImLosingMyMind("name","i really want to go eat me hungy");
         p.addDocu(d1);
         p.procesDocu();
--- Singleton Pattern/src/PRINTSPOOOLERLELERLELLERLER/SpooleOfThePrint.java
+++ Singleton Pattern/src/PRINTSPOOOLERLELERLELLERLER/SpooleOfThePrint.java
@@ -7,6 +7,15 @@ public class SpooleOfThePrint {
     private static SpooleOfThePrint instance;
     private Queue<CanYouTellImLosingMyMind> doculist;
 
+
+    public static SpooleOfThePrint getInstance(){
+        if(instance == null){
+            synchronized (SpooleOfThePrint.class){ //so no multithread classing instancecs
+                instance = new SpooleOfThePrint();
+            }
+        }
+        return instance;
+    }
     public SpooleOfThePrint() {
         this.doculist = new LinkedList<>();
     }
